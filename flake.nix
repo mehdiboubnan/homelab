951f6a86a6d05a1f0e6f276964e23a568126384f
Mehdi Boubnan <mehdi@mindee.co>
2025-01-17 22:16:54 +0100

    feat: :sparkles: first nix config

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "MehLab";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs:
+  let
+      helpers = import ./flakeHelpers.nix inputs;
+      inherit (helpers) mkMerge mkNixos mkDarwin;
+  in
+  {
+    nixosConfigurations = {
+      iso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({
+            users.users.nixos = {
+              openssh.authorizedKeys.keys = [
+                "ssh-ed25519 PUBLIC SSH KEY"
+              ];
+            };
+          })
+        ];
+      };
+      mehlaba = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs outputs;};
+      };
+    };
+  };
+}
---
+++ flakeHelpers.nix
@@ -0,0 +1,72 @@
+inputs:
+let
+  homeManagerCfg = userPackages: extraImports: {
+    home-manager.useGlobalPkgs = false;
+    home-manager.extraSpecialArgs = {
+      inherit inputs;
+    };
+    home-manager.users.notthebee.imports = [
+      inputs.agenix.homeManagerModules.default
+      inputs.nix-index-database.hmModules.nix-index
+      ./users/notthebee/dots.nix
+      ./users/notthebee/age.nix
+    ] ++ extraImports;
+    home-manager.backupFileExtension = "bak";
+    home-manager.useUserPackages = userPackages;
+  };
+in
+{
+
+  mkDarwin = machineHostname: nixpkgsVersion: extraHmModules: extraModules: {
+    darwinConfigurations.${machineHostname} = inputs.nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      specialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        "${inputs.secrets}/default.nix"
+        inputs.agenix.darwinModules.default
+        ./machines/darwin
+        ./machines/darwin/${machineHostname}
+        inputs.home-manager.darwinModules.home-manager
+        (inputs.nixpkgs.lib.attrsets.recursiveUpdate (homeManagerCfg true extraHmModules) {
+          home-manager.users.notthebee.home.homeDirectory = inputs.nixpkgs.lib.mkForce "/Users/notthebee";
+        })
+      ];
+    };
+  };
+  mkNixos = machineHostname: nixpkgsVersion: extraModules: rec {
+    deploy.nodes.${machineHostname} = {
+      hostname = machineHostname;
+      profiles.system = {
+        user = "root";
+        sshUser = "notthebee";
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos nixosConfigurations.${machineHostname};
+      };
+    };
+    nixosConfigurations.${machineHostname} = nixpkgsVersion.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+        vars = import ./machines/nixos/vars.nix;
+      };
+      modules = [
+        ./homelab
+        ./machines/nixos/common
+        ./machines/nixos/${machineHostname}
+        ./modules/email
+        ./modules/tg-notify
+        ./modules/duckdns
+        ./modules/auto-aspm
+        ./modules/mover
+        "${inputs.secrets}/default.nix"
+        inputs.agenix.nixosModules.default
+        ./users/notthebee
+        (homeManagerCfg false [ ])
+      ] ++ extraModules;
+    };
+  };
+  mkMerge = inputs.nixpkgs.lib.lists.foldl' (
+    a: b: inputs.nixpkgs.lib.attrsets.recursiveUpdate a b
+  ) { };
+}
