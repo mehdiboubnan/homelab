56f2d67f819315a296c0e547ee840f609c5b5c92
Mehdi Boubnan <mehdi@mindee.co>
2025-01-21 23:49:06 +0100

    feat: :sparkles: add braka overall config

---
+++ machines/braka/configuration.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  outputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/base/user.nix
+    ./../../modules/base/gen-initrd-sops-keys.nix
+    ./../../modules/base/remote-unlock.nix
+    ./../../modules/base/sops.nix
+  ];
+}
+  # Machine-specific secrets if needed
+#  sops.secrets.machine_specific_secret = {
+#    sopsFile = ./secrets.yaml;
+#  };
+
+#  home-manager = {
+#    extraSpecialArgs = {inherit inputs outputs;};
+#    useGlobalPkgs = true;
+#    useUserPackages = true;
+#    users = {
+#      eh8 = {
+#        imports = [
+#          ./../../modules/home-manager/base.nix
+#        ];
+#      };
+#    };
+#  };
+
+#  networking.hostName = "svr2chng";
+#}
