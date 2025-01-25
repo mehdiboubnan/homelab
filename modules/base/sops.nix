ba0b471f081e4e0903c023d1de267a5dff394c76
Mehdi Boubnan <mehdi@mindee.co>
2025-01-25 17:55:09 +0100

    refactor: :recycle: clean sops.nix

--- modules/base/sops.nix
+++ modules/base/sops.nix
@@ -27,19 +27,3 @@ in
     };
 
 }
-#  sops = {
-#    age.keyFile = "/var/lib/sops-nix/key.txt";
-    # This will add secrets.yml to the nix store
-    # You can avoid this by adding a string to the full path instead, i.e.
-    # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-#    defaultSopsFile = ./secrets/secrets.yaml;
-    # This will automatically import SSH keys as age keys
-#    age.sshKeyPaths = [ "/etc/ssh/keys/age_key.txt" ];
-    # This is using an age key that is expected to already be in the filesystem
-#    age.keyFile = "/var/lib/sops-nix/key.txt";
-    # This will generate a new key if the key specified above does not exist
-#    age.generateKey = true;
-    # This is the actual specification of the secrets.
-#    secrets.example-key = {};
-#    secrets."myservice/my_subdir/my_secret" = {};
-#  };
