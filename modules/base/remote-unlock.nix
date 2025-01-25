aad4fef899183a36e690f26d8347c24203f7dd14
Mehdi Boubnan <mehdi@mindee.co>
2025-01-25 17:54:59 +0100

    feat: :sparkles: add initrd network inferface

--- modules/base/remote-unlock.nix
+++ modules/base/remote-unlock.nix
@@ -1,7 +1,16 @@
 {config, ...}: {
-  boot.kernelParams = ["ip=dhcp"];
+#  boot.kernelParams = ["ip=dhcp"];
   boot.initrd.network = {
     enable = true;
+    # Specify the network interface name
+    interfaces = [ "enp2s0" ];  # Replace with your actual interface name
+    configurationType = "static";
+    static = {
+      ip = "192.168.0.236";  # Your desired static IP
+      gateway = "192.168.0.1";  # Gateway IP
+      netmask = "255.255.255.0";  # Subnet mask
+      dns = [ "1.1.1.1" "8.8.8.8" ];  # DNS servers
+    };
     ssh = {
       enable = true;
       port = 22;
